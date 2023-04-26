41bfc3a5b0de7dcc5e1b04a75bf2663ae9bd765f
Raquel Fernandes <94188045+fernandesraquel@users.noreply.github.com>
2023-04-26 17:21:45 -0300

    Update consultas-aninhadas.sql

--- tarefas/consultas-aninhadas.sql
+++ tarefas/consultas-aninhadas.sql
@@ -1,10 +1,19 @@
+SET FOREIGN_KEY_CHECKS=0; 
+
 -- Criando a tabela de funcionário
 CREATE TABLE funcionario (
-  codigo INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  nome VARCHAR(50) NOT NULL,
-  sexo CHAR(1) NOT NULL,
-  dt_nasc DATE NOT NULL,
-  salario DECIMAL(10, 2) NOT NULL,
-  cod_depto INT NOT NULL,
+  codigo int AUTO_INCREMENT PRIMARY KEY,
+  nome VARCHAR(50), 
+  sexo CHAR(1),
+  dt_nasc DATE,
+  salario DECIMAL(10, 2),
+  cod_depto INT,
   FOREIGN KEY (cod_depto) REFERENCES departamento(codigo)
 );
+
+-- Inserindo um novo funcionário
+INSERT INTO funcionario (nome, sexo, dt_nasc, salario, cod_depto)
+VALUES ('José', 'M', '1990-01-01', 5000.00, 1),
+       ('Raquel', 'F', '1987-02-05', 3000.00, 2),
+       ('Annielly', 'F', '1989-06-02', 7000.00, 1),
+       ('Fábio', 'M', '1992-02-08', 1000.00, 1);
